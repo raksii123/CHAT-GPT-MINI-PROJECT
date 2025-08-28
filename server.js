3997709dae3a43aa166cb2649aef26a7eacaa491
rakshii123 <rakshithghiremath028@gmail.com>
2025-08-28 12:31:11 +0530

    commit
    added socket middelware its a bit basic and dont cover all edge cases make sure to complete it at the end

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const initSocketServer = require('./src/sockets/sockets.server')
 connectDB()
 initSocketServer(httpServer)
 
-app.listen(3000, () => {
+httpServer.listen(3000, () => {
     console.log('Server is running on port 3000')
 });
--- src/sockets/sockets.server.js
+++ src/sockets/sockets.server.js
@@ -1,10 +1,31 @@
 const { Server } = require("socket.io");
+const cookie = require('cookie')
+const jwt = require('jsonwebtoken')
+const userModel = require('../models/user.model')
 
-function initSocketServer(httpServer){
+function initSocketServer(httpServer) {
 
-    
+
     const io = new Server(httpServer, {});
-    
+    io.use(async (socket, next) => {
+        const cookies = cookie.parse(socket.handshake.headers?.cookie) || "";
+
+        if (!cookies.token) {
+            next(new Error('Authentication error'))
+        }
+        try {
+            const decoded = jwt.verify(cookies.token, process.env.JWT_SECRET);
+            const user = await userModel.findById(decoded.id)
+            socket.user = user
+            next()
+        } catch (error) {
+            next(new Error("invalid token"))
+        }
+    })
+
+
+
+
     io.on("connection", (socket) => {
         console.log('socket connected', socket.id)
     });
